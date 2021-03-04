52e3dec732ba6b80eff7d03cab3cbeadaa355446
DysCo <49633591+DysCo72@users.noreply.github.com>
2021-03-04 09:47:08 -0800

    Add files via upload

---
+++ lab1.c
@@ -0,0 +1,17 @@
+/*-------------------
+ * Lab 1            */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void)
+{
+    printf("\nLab 1. \n\n");
+    printf("Hello World.\n");
+    printf("Hi, XXXXXX. \n\n");
+    printf("Intelligence is the ability to adapt to change.\n");
+    printf("-Stephen Hawking.\n\n");
+
+    return EXIT_SUCCESS;
+}
+//-------------------
